55f1468051ed08d8b5e4160c9fc943fcc95338d9
PriyankaMane1772 <priyumane13@gmail.com>
2024-07-01 01:23:13 +0530

    first commit

---
+++ server/models/employee.js
@@ -0,0 +1,11 @@
+const { default: mongoose } = require('mongoose')
+const mongose = require('mongoose')
+
+const EmployeeSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String
+})
+
+const EmployeeModel = mongoose.model("employees", EmployeeSchema)
+module.exports = EmployeeModel
